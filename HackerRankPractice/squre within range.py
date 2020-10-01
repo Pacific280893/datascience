dc5cddb865ee8f9bf76bcfd770ba9d7269b01d8f
Pacific280893 <yadavkprashant1993@gmail.com>
2020-10-02 01:15:48 +0530

    square

---
+++ HackerRankPractice/squre within range.py
@@ -0,0 +1,8 @@
+"""The provided code stub reads and integer,n , from STDIN. For all non-negative integers i <n , print n^2."""
+n = 4
+[print(i**2) for i in range(n)]
+
+"""or in second way"""
+
+for i in range(n):
+    print(i**2)
