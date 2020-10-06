6b88da6855462e5dc0e62a7fb196c663381280ab
Pacific280893 <yadavkprashant1993@gmail.com>
2020-10-07 01:09:25 +0530

    test

--- HackerRankPractice/minion game.py
+++ HackerRankPractice/minion game.py
@@ -1,4 +1,4 @@
-s = input()
+"""s = input()
 
 vowels = 'AEIOU'
 
@@ -15,4 +15,25 @@ if kevsc > stusc:
 elif kevsc < stusc:
     print ("Stuart", stusc)
 else:
-    print ("Draw")
+    print ("Draw")"""
+
+"""actual ans"""
+def minion_game(string):
+    vowel =['A','E','I','O','U']
+    S=0
+    K=0
+    for i in range(len(string)):
+        if string[i] in vowel:
+            K+= len(string)-i
+        else:
+            S+=len(string)-i
+    if S>K:
+        print("Stuart"+" "+ "%d" % S)
+    elif K>S:
+        print("Kevin"+" "+'%d' % K)
+    else:
+        print("Draw")
+
+
+string = 'BANANA'
+print(minion_game(string))
